46d64256b3dd446fd25cef0ca25018c96df038a2
Selemin Yuriy <71262890+yuriselemin@users.noreply.github.com>
2024-08-01 20:45:59 +0300

    Add files via upload

--- MOD-9_module_9_4.py
+++ MOD-9_module_9_4.py
@@ -1,26 +1,22 @@
+from random import choice
 
 # Домашнее задание по теме "Создание функций на лету"
-
-
-
-from random import choice
 
 
 first = 'Мама мыла раму'
 second = 'Рамена мало было'
 
-print(list(map(lambda x, y: x == y, first, second)))
+match_letters = list(map(lambda x, y: x == y, first, second))
+print(match_letters)
 
 
-# Замыкание
+
 def get_advanced_writer(file_name):
     def write_everything(*data_set):
-        with open(file_name, 'w', encoding='utf-8') as file:
-            for i in data_set:
-                file.write(str(i) + '\n')
-
+        with open(file_name, 'a', encoding='utf-8') as file:
+            for data in data_set:
+                file.write(str(data) + '\n')
     return write_everything
-
 
 write = get_advanced_writer('example.txt')
 write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
@@ -38,6 +34,3 @@ first_ball = MysticBall('Да', 'Нет', 'Наверное')
 print(first_ball())
 print(first_ball())
 print(first_ball())
-
-
-
